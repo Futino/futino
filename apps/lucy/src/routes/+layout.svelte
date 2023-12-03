e14332338df79ad7e2e0c8521d2d7e5a080b5d5c
Daniel Aanensen <calibor@protonmail.com>
2023-12-03 18:55:35 +0100

    price bar

--- apps/lucy/src/routes/+layout.svelte
+++ apps/lucy/src/routes/+layout.svelte
@@ -2,12 +2,9 @@
   import "../app.postcss";
 </script>
 
-<slot />
-
 <div
-  class="bg-landing w-full h-screen bg-cover bg-center flex justify-center items-center"
+  class="flex-col gap-4 bg-landing w-full h-screen bg-cover bg-center flex justify-center items-center"
 >
   <h1 class="text-emerald-300 text-5xl">bunny</h1>
+  <slot />
 </div>
-
-bunny
--- apps/lucy/src/routes/+page.svelte
+++ apps/lucy/src/routes/+page.svelte
@@ -1,3 +1,18 @@
 <script lang="ts">
-  import { text } from "@sveltejs/kit";
+  const hourlyRate = 25.45;
+  const cogs = 5.35;
+  const markup = 1.4; // 40%
+  const maintenanceHours = 1;
+
+  let chosenHours = 5;
+
+  $: hours = chosenHours + maintenanceHours;
+  $: expenses = hours * hourlyRate + cogs;
+  $: markedUp = expenses * markup;
+  // Calculate stripe fees
+  $: fees = markedUp * 0.0713 + 0.6;
+  $: price = markedUp + fees;
 </script>
+
+<input type="number" name="chosenHours" bind:value={chosenHours} />
+<p>{price}</p>
