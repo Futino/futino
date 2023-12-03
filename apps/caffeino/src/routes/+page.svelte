1659e617066ce979cd0802061e1b48cc50575f86
Jonas Lindberg <eksno@protonmail.com>
2023-12-03 20:57:10 +0100

    working slider

--- apps/caffeino/src/routes/+page.svelte
+++ apps/caffeino/src/routes/+page.svelte
@@ -1,18 +1,2 @@
 <script lang="ts">
-  const hourlyRate = 25.45;
-  const cogs = 5.35;
-  const markup = 1.4; // 40%
-  const maintenanceHours = 1;
-
-  let chosenHours = 5;
-
-	$: hours = chosenHours + maintenanceHours;
-	$: expenses = hours * hourlyRate + cogs;
-	$: markedUp = expenses * markup;
-	// Calculate stripe fees
-	$: fees = markedUp * 0.0713 + 0.6;
-	$: price = markedUp + fees;
 </script>
-
-<input type="number" name="chosenHours" bind:value={chosenHours} />
-<p>{price}</p>
--- apps/caffeino/src/routes/pricing/+page.svelte
+++ apps/caffeino/src/routes/pricing/+page.svelte
@@ -5,20 +5,28 @@
 	const markup = 1.4; // 40%
 	const maintenanceHours = 1;
 
-	$: chosenHours = [1];
+	const currencyFormatter = new Intl.NumberFormat('en-US', {
+		style: 'currency',
+		currency: 'USD',
 
-	$: hours = chosenHours[0] + maintenanceHours;
+		// These options are needed to round to whole numbers if that's what you want.
+		minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
+		maximumFractionDigits: 0 // (causes 2500.99 to be printed as $2,501)
+	});
+
+	$: slider = [1];
+	$: chosenHours = Math.floor(slider[0] / Math.max(0.1, 10 - slider[0] / 20));
+	$: hours = chosenHours + maintenanceHours;
 	$: expenses = hours * hourlyRate + cogs;
 	$: markedUp = expenses * markup;
-	// Calculate stripe fees
 	$: fees = markedUp * 0.0713 + 0.6;
 	$: price = markedUp + fees;
 </script>
 
 <div class="flex flex-col gap-12 items-center justify-center h-screen w-full">
 	<div class="flex gap-6 text-5xl">
-		<p>{hours} hours</p>
-		<p>${price}<span class="text-sm">per month</span></p>
+		<p>{chosenHours} hours</p>
+		<p>{currencyFormatter.format(price)}<span class="text-sm">per month</span></p>
 	</div>
-	<Slider bind:value={chosenHours} class="max-w-2xl" max={40} min={1} step={1} />
+	<Slider bind:value={slider} class="max-w-2xl" max={134} min={0} step={1} />
 </div>
