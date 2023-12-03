66d5c54fad3520cfd00fb8c01a746de74f156fa6
Daniel Aanensen <calibor@protonmail.com>
2023-12-03 18:59:53 +0100

    calculate price

--- apps/caffeino/src/routes/+page.svelte
+++ apps/caffeino/src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  const hourlyRate = 25.45;
+  const cogs = 5.35;
+  const markup = 1.4; // 40%
+  const maintenanceHours = 1;
+
+  let chosenHours = 5;
+
+  $: hours = chosenHours + maintenanceHours;
+  $: expenses = hours * hourlyRate + cogs;
+  $: markedUp = expenses * markup;
+  // Calculate stripe fees
+  $: fees = markedUp * 0.0713 + 0.6;
+  $: price = markedUp + fees;
+</script>
+
+<input type="number" name="chosenHours" bind:value={chosenHours} />
+<p>{price}</p>
