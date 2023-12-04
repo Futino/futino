0836678ad1e15ac73f188289d2e3a62a0a6dac7c
Jonas Lindberg <eksno@protonmail.com>
2023-12-04 14:21:20 +0100

    improve slider

--- apps/caffeino/src/routes/pricing/+page.svelte
+++ apps/caffeino/src/routes/pricing/+page.svelte
@@ -3,7 +3,8 @@
 	const hourlyRate = 25.45;
 	const cogs = 5.35;
 	const markup = 1.4; // 40%
-	const maintenanceHours = 1;
+	const minHours = 1;
+	const maxHours = 40;
 
 	const currencyFormatter = new Intl.NumberFormat('en-US', {
 		style: 'currency',
@@ -14,9 +15,10 @@
 		maximumFractionDigits: 0 // (causes 2500.99 to be printed as $2,501)
 	});
 
-	$: slider = [1];
-	$: chosenHours = Math.floor(slider[0] / Math.max(0.1, 10 - slider[0] / 20));
-	$: hours = chosenHours + maintenanceHours;
+	$: slider = [0];
+	$: hours = Math.floor(
+		((12000 / (slider[0] - 600)) * -1 - 20) * (maxHours / 100) * (1 - minHours / 100) + minHours
+	);
 	$: expenses = hours * hourlyRate + cogs;
 	$: markedUp = expenses * markup;
 	$: fees = markedUp * 0.0713 + 0.3;
@@ -25,8 +27,8 @@
 
 <div class="flex flex-col gap-12 items-center justify-center h-screen w-full">
 	<div class="flex gap-6 text-5xl">
-		<p>{chosenHours} hours</p>
+		<p>{hours} hours</p>
 		<p>{currencyFormatter.format(price)}<span class="text-sm">per month</span></p>
 	</div>
-	<Slider bind:value={slider} class="max-w-2xl" max={134} min={0} step={1} />
+	<Slider bind:value={slider} class="max-w-2xl" max={500} min={0} step={1} />
 </div>
