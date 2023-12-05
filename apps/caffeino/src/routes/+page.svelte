12132c8dd09b1bfebb83f830922e08623d7f14c8
Jonas Lindberg <eksno@protonmail.com>
2023-12-05 19:29:29 +0100

    setup caffeino

--- apps/caffeino/src/routes/+page.svelte
+++ apps/caffeino/src/routes/+page.svelte
@@ -1,2 +1,49 @@
 <script lang="ts">
+	import * as Card from '$lib/components/ui/card';
 </script>
+
+<main class="flex flex-col justify-center items-center bg-background text-white">
+	<!-- Hero -->
+	<section class="h-screen w-screen bg-gradient-to-b from-secondary to-background" />
+
+	<section class="max-w-5xl grid grid-cols-4 gap-4">
+		<Card.Root>
+			<Card.Header>
+				<Card.Title>Design</Card.Title>
+				<Card.Description>
+					Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint
+					consectetur cupidatat.
+				</Card.Description>
+			</Card.Header>
+		</Card.Root>
+		<Card.Root>
+			<Card.Header>
+				<Card.Title>Design</Card.Title>
+				<Card.Description>
+					Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint
+					consectetur cupidatat.
+				</Card.Description>
+			</Card.Header>
+		</Card.Root>
+		<Card.Root>
+			<Card.Header>
+				<Card.Title>Design</Card.Title>
+				<Card.Description>
+					Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint
+					consectetur cupidatat.
+				</Card.Description>
+			</Card.Header>
+		</Card.Root>
+		<Card.Root>
+			<Card.Header>
+				<Card.Title>Design</Card.Title>
+				<Card.Description>
+					Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint
+					consectetur cupidatat.
+				</Card.Description>
+			</Card.Header>
+		</Card.Root>
+	</section>
+
+	<section class="h-screen" />
+</main>
